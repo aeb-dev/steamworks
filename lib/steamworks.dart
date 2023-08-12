ea2f060ae34a5e699090dafccff6e15c2bef8df0
Ahmet Enes Bayraktar <bayraktarahmetenes@gmail.com>
2023-08-12 15:01:08 +0300

    dont hide steam apis

--- lib/steamworks.dart
+++ lib/steamworks.dart
@@ -1,5 +1,5 @@
 export "src/call_result.dart";
 export "src/callback.dart";
-export "src/generated/generated.dart" hide Dispatch, SteamApi, SteamGameServer;
+export "src/generated/generated.dart";
 export "src/steam_client.dart";
 export "src/steam_server.dart";
